dd433199499c06d6c12e163cd51c5fb4ce210ffc
Elena <Elena@EquipoElena>
2018-01-29 21:34:57 +0100

    segunda modificacion

--- pruebaRepositorio/11_pruebaGit/src/Prueba.java
+++ pruebaRepositorio/11_pruebaGit/src/Prueba.java
@@ -3,5 +3,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("...............");
+		System.out.println("modificacion");
 	}   
 }
